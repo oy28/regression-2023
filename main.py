2e70d94644a4446a9143d3f83032fe7ce615376a
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2023-11-02 11:40:36 +0900

    ランダムにサンプルした20点についてsin(πx)を表示した

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import numpy as np
 def main():
     x = np.linspace(-1, 1, 101)
     y = np.sin(np.pi * x)
+    sample_x = np.random.uniform(-1, 1, (20, ))
+    sample_y = np.sin(np.pi * sample_x)
     fig = Figure()
     ax = fig.add_subplot(1, 1, 1, xlabel='$x$', ylabel='$y$')
     ax.set_title(r'$y = \sin (\pi x)$')
     ax.axhline(0, color='#777777')
     ax.axvline(0, color='#777777')
     ax.plot(x, y)
+    ax.scatter(sample_x, sample_y, color='red')
     fig.savefig('out.png')
 
 if __name__ == '__main__':
