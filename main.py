e3bed2d496ff9a038dc168d4b128952b12cf6c4c
fukuzaki <fukuzaki@tkhm.elec.keio.ac.jp>
2023-11-02 11:34:57 +0900

    グラフの見た目を整えた

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ def main():
     x = np.linspace(-1, 1, 101)
     y = np.sin(np.pi * x)
     fig = Figure()
-    ax = fig.add_subplot(1, 1, 1)
+    ax = fig.add_subplot(1, 1, 1, xlabel='$x$', ylabel='$y$')
+    ax.set_title(r'$y = \sin (\pi x)$')
+    ax.axhline(0, color='#777777')
+    ax.axvline(0, color='#777777')
     ax.plot(x, y)
     fig.savefig('out.png')
 
