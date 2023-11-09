ba8a74b24c356c7a54ea39df59142705e8cad5fa
oy28 <odayuma28@keio.jp>
2023-11-09 11:14:49 +0900

    スコア計算、グラフ出力を関数化した
    グラフは一部を非表示できるようにした

--- main.py
+++ main.py
@@ -2,6 +2,38 @@ from matplotlib.figure import Figure
 import japanize_matplotlib as _
 import numpy as np
 
+def calculate_score(y, y_pred, score_eps):
+    norm_diff = np.sum(np.abs(y-y_pred))
+    norm_y = np.sum(np.abs(y))
+    score = norm_diff/(norm_y + score_eps)
+    return score
+
+def save_graph(
+        xy=None,
+        xy_sample=None,
+        xy_pred=None,
+        title=None,
+        filename='out.png'
+):
+    fig = Figure()
+    ax = fig.add_subplot(1, 1, 1, xlabel='$x$', ylabel='$y$')
+    if title is not None:
+        ax.set_title(title)
+    ax.axhline(0, color='#777777')
+    ax.axvline(0, color='#777777')
+    if xy is not None:
+        x,y=xy
+        ax.plot(x, y, color='blue',label='真の関数 $f$')
+    if xy_sample is not None:
+        sample_x,sample_y =xy_sample
+        ax.scatter(sample_x, sample_y, color='red', label='学習サンプル')
+    if xy_pred is not None:
+        x_pred, y_pred = xy_pred
+        ax.plot(x_pred, y_pred, color='orange', label='回帰関数 $\hat{f}$')
+    ax.legend()
+    fig.savefig(filename)
+    
+
 def main():
     #実験条件
     x_min=-1
@@ -12,7 +44,7 @@ def main():
     score_eps=1e-8
     #多項式フィッティングの条件
     d=3
-    
+
     #変数の準備
     x = np.linspace(x_min, x_max, n_test)
     y = np.sin(np.pi * x)
@@ -30,21 +62,14 @@ def main():
     X = x[:,np.newaxis] ** p[np.newaxis, :]
     y_pred = np.squeeze(X @ a)
     # 評価指標の算出
-    norm_diff = np.sum(np.abs(y-y_pred))
-    norm_y = np.sum(np.abs(y))
-    score = norm_diff/(norm_y + score_eps)
+    score=calculate_score(y,y_pred,score_eps)
     print(f'{score=:.3f}')
     #グラフの表示
-    fig = Figure()
-    ax = fig.add_subplot(1, 1, 1, xlabel='$x$', ylabel='$y$')
-    ax.set_title(r'$y = \sin (\pi x)$')
-    ax.axhline(0, color='#777777')
-    ax.axvline(0, color='#777777')
-    ax.plot(x, y, label='真の関数 $f$')
-    ax.scatter(sample_x, sample_y, color='red', label='学習サンプル')
-    ax.plot(x, y_pred, label='回帰関数 $\hat{f}$')
-    ax.legend()
-    fig.savefig('out.png')
+    save_graph(
+        xy=(x,y),xy_sample=(sample_x,sample_y),xy_pred=(x,y_pred),
+        title=r'$y = \sin (\pi x)$'
+    )
+    
 
 if __name__ == '__main__':
     main()
