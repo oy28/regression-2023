2d2f01a8094951f0f83a9882acf9f450a3cb194e
oy28 <odayuma28@keio.jp>
2023-11-09 09:47:03 +0900

    yの値を多項式フィッティングで予測して表示した

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ def main():
     sample_x = np.random.uniform(-1, 1, (20, ))
     sample_noise = np.random.normal(0, y_range*0.05, (20, ))
     sample_y = np.sin(np.pi * sample_x) + sample_noise
+    # 多項式フィッティング
+    ## 学習サンプルから係数を求める
+    d = 3
+    p = np.arange(d+1)
+    sample_X = sample_x[:, np.newaxis] ** p[np.newaxis, :]
+    sample_XX_inv = np.linalg.inv(sample_X.T @ sample_X)
+    a = sample_XX_inv @ sample_X.T @ sample_y[:, np.newaxis]
+    ##求めた係数を用いてyの値を表示
+    X = x[:,np.newaxis] ** p[np.newaxis, :]
+    y_pred = np.squeeze(X @ a)
+    #グラフの表示
     fig = Figure()
     ax = fig.add_subplot(1, 1, 1, xlabel='$x$', ylabel='$y$')
     ax.set_title(r'$y = \sin (\pi x)$')
@@ -16,6 +27,7 @@ def main():
     ax.axvline(0, color='#777777')
     ax.plot(x, y, label='真の関数 $f$')
     ax.scatter(sample_x, sample_y, color='red', label='学習サンプル')
+    ax.plot(x, y_pred, label='回帰関数 $\hat{f}$')
     ax.legend()
     fig.savefig('out.png')
 
